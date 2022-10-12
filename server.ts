206d0d23063a683d8de03a954813649bf7c63a2d
kemal <kemaldeniz.teket@gmail.com>
2022-10-12 17:08:03 +0300

    first deploy

--- server.ts
+++ server.ts
@@ -55,7 +55,7 @@ exxApp.use((req: express.Request, res: express.Response, next: express.NextFunct
     res.header('Access-Control-Allow-Headers', 'origin, X-Requested-With,Content-Type,Accept, Authorization');
     // set the CORS method headers
     if (req.method === 'OPTIONS') {
-        res.header('Access-Control-Allow-Methods', 'GET PUT DELETE POST');
+        res.header('Access-Control-Allow-Methods', 'GET, HEAD, OPTIONS, PUT, DELETE, POST');
         return res.status(200).json({});
     }
     next();
